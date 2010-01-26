d60c58f8e7232da989a9f9f366f7916f8168d35d
Christian Schepp Schaefer <schaepp@gmx.de>
2010-01-26 15:58:44 +0100

    Wrapped everything inside a function
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 error_reporting(2047);
 
-$stretch = (isset($_POST['stretch']) && intval($_POST['stretch']) > 1) ? intval($_POST['stretch']) : 1;
-$prefix = (isset($_POST['prefix']) && preg_match('/[a-z]/i',$_POST['prefix']) == 1) ? $_POST['prefix'] : 't';
-$border = FALSE;
-if($border && $stretch > 1) $style = 'table.'.$prefix.'table{border-right: #000 1px solid;border-top: #000 1px solid}
-table.'.$prefix.'table td{{width:'.($stretch - 1).'px;height:'.($stretch - 1).'px;border-left: #000 1px solid;border-bottom: #000 1px solid;}
-';
-else $style = 'table.'.$prefix.'table td{width:'.$stretch.'px;height:'.$stretch.'px;}
-';
-$table = '';
-
-if(isset($_FILES['userfile']))
+function img2table($sourcefile = '',$stretch = 1,$prefix = 't')
 {
+	$style = 'table.'.$prefix.'table td{width:'.$stretch.'px;height:'.$stretch.'px;}'."\n";
+	$table = '';
 	$backgroundcolor = '';
 	$class_array = array();
 	$class_count = array();
@@ -29,68 +21,78 @@ if(isset($_FILES['userfile']))
 			else break;
 		}
 	}
-	
+
+	$info = getimagesize($sourcefile);
+	if(strtoupper($info[2]) == 1 || strtoupper($info[2]) == 2 || strtoupper($info[2]) == 3)
+	{
+		if(strtoupper($info[2]) == 1) $img = imagecreatefromgif($sourcefile);
+		if(strtoupper($info[2]) == 2) 
+		{
+			$img = imagecreatefromjpeg($sourcefile);
+			imagetruecolortopalette($img, false, 255);
+		}
+		if(strtoupper($info[2]) == 3) 
+		{
+			$img = imagecreatefrompng($sourcefile);
+			imagetruecolortopalette($img, false, 255);
+		}
+		for($y=0;$y<$info[1];$y++)
+		{
+			$lastcolor = '';
+			$colorcounter = 0;
+			if($y > 0) $table .= "<tr>";
+			for($x=0;$x<$info[0];$x++)
+			{
+				$currentcolor = imagecolorat($img, $x, $y);
+				$colorrgb = imagecolorsforindex($img,$currentcolor);
+				$currentcolor = '#'.str_pad(dechex($colorrgb['red']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['green']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['blue']),2,'0',STR_PAD_LEFT);
+				if(substr($currentcolor,1,1) == substr($currentcolor,2,1) && substr($currentcolor,3,1) == substr($currentcolor,4,1) && substr($currentcolor,5,1) == substr($currentcolor,6,1))
+				{
+					$currentcolor = '#'.substr($currentcolor,1,1).substr($currentcolor,3,1).substr($currentcolor,5,1);
+				}
+				if(!isset($class_array[$currentcolor]))
+				{
+					$classname = $prefix.$class_table[count($class_array)];
+					$class_array[$currentcolor] = $classname;
+					$style .= ".".$classname."{background:".$currentcolor."}\n";
+				}
+				
+				if($x == 0) $lastcolor = $currentcolor;
+				
+				if(($y == 0 && $x != 0) || ($colorcounter > 0 && $lastcolor != $currentcolor)) 
+				{
+					if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$lastcolor].">";
+					else $table .= "<td class=".$class_array[$lastcolor].">";
+					if(!isset($class_count[$currentcolor])) $class_count[$currentcolor] = 1;
+					else $class_count[$currentcolor]++;
+					$lastcolor = $currentcolor;
+					$colorcounter = 0;
+				}
+				$colorcounter++;
+			}
+			if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$currentcolor].">";
+			elseif($colorcounter == 1) $table .= "<td class=".$class_array[$currentcolor].">";
+		}
+		$table .= "</table>";
+		arsort($class_count);
+		reset($class_count);
+		$backgroundcolor = key($class_count);
+		$table = "<table cellpadding=0 cellspacing=0 border=0 width=".($info[0] * $stretch)." class=".$prefix."table style=table-layout:fixed bgcolor=".$backgroundcolor."><tr>".str_replace(" class=".$class_array[$backgroundcolor].">",">",$table);
+		return "<style type=\"text/css\">\n".$style."</style>\n".$table;
+	}
+	else return 'Unsupported file format!';
+}
+
+$stretch = (isset($_POST['stretch']) && intval($_POST['stretch']) > 1) ? intval($_POST['stretch']) : 1;
+$prefix = (isset($_POST['prefix']) && preg_match('/[a-z]/i',$_POST['prefix']) == 1) ? $_POST['prefix'] : 't';
+$html = '';
+if(isset($_FILES['userfile']))
+{
 	$uploadfile = $_FILES['userfile']['name'];
 	if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) 
 	{
+		$html = img2table($uploadfile,$stretch,$prefix);
 		$info = getimagesize($uploadfile);
-		if(strtoupper($info[2]) == 1 || strtoupper($info[2]) == 2 || strtoupper($info[2]) == 3)
-		{
-			if(strtoupper($info[2]) == 1) $img = imagecreatefromgif($uploadfile);
-			if(strtoupper($info[2]) == 2) 
-			{
-				$img = imagecreatefromjpeg($uploadfile);
-				imagetruecolortopalette($img, false, 255);
-			}
-			if(strtoupper($info[2]) == 3) 
-			{
-				$img = imagecreatefrompng($uploadfile);
-				imagetruecolortopalette($img, false, 255);
-			}
-			for($y=0;$y<$info[1];$y++)
-			{
-				$lastcolor = '';
-				$colorcounter = 0;
-				if($y > 0) $table .= "<tr>";
-				for($x=0;$x<$info[0];$x++)
-				{
-					$currentcolor = imagecolorat($img, $x, $y);
-					$colorrgb = imagecolorsforindex($img,$currentcolor);
-					$currentcolor = '#'.str_pad(dechex($colorrgb['red']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['green']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['blue']),2,'0',STR_PAD_LEFT);
-					if(substr($currentcolor,1,1) == substr($currentcolor,2,1) && substr($currentcolor,3,1) == substr($currentcolor,4,1) && substr($currentcolor,5,1) == substr($currentcolor,6,1))
-					{
-						$currentcolor = '#'.substr($currentcolor,1,1).substr($currentcolor,3,1).substr($currentcolor,5,1);
-					}
-					if(!isset($class_array[$currentcolor]))
-					{
-						$classname = $prefix.$class_table[count($class_array)];
-						$class_array[$currentcolor] = $classname;
-						$style .= ".".$classname."{background:".$currentcolor."}\n";
-					}
-					
-					if($x == 0) $lastcolor = $currentcolor;
-					
-					if(($y == 0 && $x != 0) || ($colorcounter > 0 && $lastcolor != $currentcolor)) 
-					{
-						if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$lastcolor].">";
-						else $table .= "<td class=".$class_array[$lastcolor].">";
-						if(!isset($class_count[$currentcolor])) $class_count[$currentcolor] = 1;
-						else $class_count[$currentcolor]++;
-						$lastcolor = $currentcolor;
-						$colorcounter = 0;
-					}
-					$colorcounter++;
-				}
-				if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$currentcolor].">";
-				elseif($colorcounter == 1) $table .= "<td class=".$class_array[$currentcolor].">";
-			}
-			$table .= "</table>";
-			arsort($class_count);
-			reset($class_count);
-			$backgroundcolor = key($class_count);
-			$table = "<table cellpadding=0 cellspacing=0 border=0 width=".($info[0] * $stretch)." class=".$prefix."table style=table-layout:fixed bgcolor=".$backgroundcolor."><tr>".str_replace(" class=".$class_array[$backgroundcolor].">",">",$table);
-		}
-		else echo 'Unsupported file format!';
 	}
 }
 ?>
@@ -102,7 +104,7 @@ if(isset($_FILES['userfile']))
 <style type="text/css">
 body {font-family: Arial, Helvetica, sans-serif; background-color: #666; color: #FFF;}
 table {border: #333333 1px solid;}
-<?php echo $style ?></style>
+</style>
 </head>
 <body>
 <h2>Options</h2>
@@ -117,13 +119,11 @@ table {border: #333333 1px solid;}
 <?php 
 if(isset($_FILES['userfile']))
 {
-	echo '<h2>Result</h2>Dimensions: '.$info[0].' &times; '.$info[1].' pixel<br />Colors Used: '.count($class_array).' colors<br />Uncompressed Byte-Size (8-Bit Color): '.round(($info[0]*$info[1])/1024).' KB<br /><br />'.$table.'<br /><h2>Size Comparison</h2>'; 
+	echo '<h2>Result</h2>Dimensions: '.$info[0].' &times; '.$info[1].' pixel<br />Uncompressed Byte-Size (8-Bit Color): '.round(($info[0]*$info[1])/1024).' KB<br /><br />'.$html.'<br /><h2>Size Comparison</h2>'; 
 	echo 'Before: '.round(filesize($uploadfile) / 1024).' KB (source-image file-size)<br />';
-	echo 'After: '.round(strlen($style.$table) / 1024).' KB (source-code byte-size)<br />';
+	echo 'After: '.round(strlen($html) / 1024).' KB (source-code byte-size)<br />';
 echo '<h2>Code</h2>
-<textarea cols="200" rows="100" style="width: 95%"><style type="text/css">
-'.$style.'</style>
-'.$table.'</textarea>';
+<textarea cols="200" rows="100" style="width: 95%">'.$html.'</textarea>';
 }
 ?>
 
