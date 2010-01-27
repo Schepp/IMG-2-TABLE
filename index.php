e93e08ed2e246d5da81c26c4a3af2c4304d772ff
Christian Schepp Schaefer <schaepp@gmx.de>
2010-01-28 00:55:36 +0100

    Added Outlook 2007 bufixes. Now fully functional in OL2007.
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 error_reporting(2047);
+include('class.phpmailer.php');
 
 function img2table($sourcefile = '',$stretch = 1,$prefix = 't')
 {
-	$style = 'table.'.$prefix.'table td{width:'.$stretch.'px;height:'.$stretch.'px;}'."\n";
+	$style = '.'.$prefix.'table td{height:'.$stretch.'px;border:0;font-size:1px}'."\n";
 	$table = '';
 	$backgroundcolor = '';
+	$charcount = 0;
+	$linelengththreshold = 700;
 	$class_array = array();
 	$class_count = array();
 	$class_table = array();
 	for($i=48;$i<58;$i++) array_push($class_table,chr($i));
 	for($i=65;$i<91;$i++) array_push($class_table,chr($i));
-	for($i=97;$i<123;$i++) array_push($class_table,chr($i));
 	$class_table_count = count($class_table);
 	for($j=0;$j<$class_table_count;$j++)
 	{
@@ -40,13 +42,14 @@ function img2table($sourcefile = '',$stretch = 1,$prefix = 't')
 		{
 			$lastcolor = '';
 			$colorcounter = 0;
-			if($y > 0) $table .= "<tr>";
+			$table .= "<tr><td><table cellpadding=0 cellspacing=0 border=0 width=".($info[0] * $stretch)." height=1><tr>";
+			$charcount = strlen($table);
 			for($x=0;$x<$info[0];$x++)
 			{
 				$currentcolor = imagecolorat($img, $x, $y);
 				$colorrgb = imagecolorsforindex($img,$currentcolor);
 				$currentcolor = '#'.str_pad(dechex($colorrgb['red']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['green']),2,'0',STR_PAD_LEFT).str_pad(dechex($colorrgb['blue']),2,'0',STR_PAD_LEFT);
-				if(substr($currentcolor,1,1) == substr($currentcolor,2,1) && substr($currentcolor,3,1) == substr($currentcolor,4,1) && substr($currentcolor,5,1) == substr($currentcolor,6,1))
+				if(1!=1 && substr($currentcolor,1,1) == substr($currentcolor,2,1) && substr($currentcolor,3,1) == substr($currentcolor,4,1) && substr($currentcolor,5,1) == substr($currentcolor,6,1))
 				{
 					$currentcolor = '#'.substr($currentcolor,1,1).substr($currentcolor,3,1).substr($currentcolor,5,1);
 				}
@@ -59,26 +62,43 @@ function img2table($sourcefile = '',$stretch = 1,$prefix = 't')
 				
 				if($x == 0) $lastcolor = $currentcolor;
 				
-				if(($y == 0 && $x != 0) || ($colorcounter > 0 && $lastcolor != $currentcolor)) 
+				if($colorcounter > 0 && $lastcolor != $currentcolor) 
 				{
-					if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$lastcolor].">";
-					else $table .= "<td class=".$class_array[$lastcolor].">";
+					$charbefore = strlen($table);
+					if($colorcounter > 1) $table .= "<td width=".($stretch * $colorcounter)." class=".$class_array[$lastcolor].">";
+					else $table .= "<td".(($stretch > 1) ? " width=".$stretch : "")." class=".$class_array[$lastcolor].">";
 					if(!isset($class_count[$currentcolor])) $class_count[$currentcolor] = 1;
 					else $class_count[$currentcolor]++;
 					$lastcolor = $currentcolor;
 					$colorcounter = 0;
+					$charafter = strlen($table);
+					$charcount += ($charafter - $charbefore);
+					if($charcount > $linelengththreshold) 
+					{
+						$table .= "\n";
+						$charcount = 0;
+					}
 				}
 				$colorcounter++;
 			}
-			if($colorcounter > 1) $table .= "<td colspan=".$colorcounter." class=".$class_array[$currentcolor].">";
-			elseif($colorcounter == 1) $table .= "<td class=".$class_array[$currentcolor].">";
+			$charbefore = strlen($table);
+			if($colorcounter > 1) $table .= "<td width=".($stretch * $colorcounter)." class=".$class_array[$currentcolor].">";
+			elseif($colorcounter == 1) $table .= "<td".(($stretch > 1) ? " width=".$stretch : "")." class=".$class_array[$currentcolor].">";
+			$table .= "</table>";
+			$charafter = strlen($table);
+			$charcount += ($charafter - $charbefore);
+			if($charcount > $linelengththreshold) 
+			{
+				$table .= "\n";
+				$charcount = 0;
+			}
 		}
 		$table .= "</table>";
 		arsort($class_count);
 		reset($class_count);
 		$backgroundcolor = key($class_count);
-		$table = "<table cellpadding=0 cellspacing=0 border=0 width=".($info[0] * $stretch)." class=".$prefix."table style=table-layout:fixed bgcolor=".$backgroundcolor."><tr>".str_replace(" class=".$class_array[$backgroundcolor].">",">",$table);
-		return "<style type=\"text/css\">\n".$style."</style>\n".$table;
+		$table = "<table cellpadding=0 cellspacing=0 border=0 width=".($info[0] * $stretch)." class=".$prefix."table bgcolor=".$backgroundcolor.">".str_replace(" class=".$class_array[$backgroundcolor].">",">",$table);
+		return "<style type=text/css>\n".$style."</style>\n".$table;
 	}
 	else return 'Unsupported file format!';
 }
@@ -88,7 +108,7 @@ $prefix = (isset($_POST['prefix']) && preg_match('/[a-z]/i',$_POST['prefix']) ==
 $html = '';
 if(isset($_FILES['userfile']) && isset($_FILES['userfile']['name']) && trim($_FILES['userfile']['name']) != '')
 {
-	$uploadfile = $_FILES['userfile']['name'];
+	$uploadfile = 'temp/'.$_FILES['userfile']['name'];
 	if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) 
 	{
 		$html = img2table($uploadfile,$stretch,$prefix);
@@ -97,35 +117,73 @@ if(isset($_FILES['userfile']) && isset($_FILES['userfile']['name']) && trim($_FI
 }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>&lt;IMG&gt; 2 &lt;TABLE&gt; Converter</title>
 <style type="text/css">
 body {font-family: Arial, Helvetica, sans-serif; background-color: #666; color: #FFF;}
-table {border: #333333 1px solid;}
 </style>
 </head>
 <body>
 <h2>Options</h2>
-<form name="bildform" action="index.php" method="post" enctype="multipart/form-data">
-	<label>Stretch-factor: <input type="text" name="stretch" value="<?php echo $stretch; ?>" size="1" maxlength="1"></label>&nbsp;
-	<label>Class-prefix: <input type="text" name="prefix" value="<?php echo $prefix; ?>" size="1" maxlength="1"></label><br />
-	<label>File (GIF/PNG/JPG): <input type="file" name="userfile" size="40"></label>
-	<input type="submit" value="convert"><br />
-	<i><strong>Note:</strong> truecolor-images will be reduced to 256 colors. Transparency is not yet supported.</i>
-</form>
+<?php
+$out = '<form name="bildform" action="'.$_SERVER['PHP_SELF'].'" method="post" enctype="multipart/form-data">
+	<label>Stretch-factor: <input type="text" name="stretch" value="'.$stretch.'" size="1" maxlength="1"></label>&nbsp;
+	<label>Class-prefix: <input type="text" name="prefix" value="'.$prefix.'" size="1" maxlength="1"></label><br>
+	<label>File (GIF/PNG/JPG): <input type="file" name="userfile" size="40"></label><br>
+	<i><strong>Note:</strong> truecolor-images will be reduced to 256 colors. Transparency is not yet supported.</i><br>
+	<label>Also send as HTML-mail to: <input type="text" name="email" value="'.((isset($_POST['email'])) ? $_POST['email'] : '').'" size="40"></label><br>
+	<input type="submit" value="convert"><br>
+</form>';
+$javascript = '<!-- SPAM protection start -->
+<script type="text/javascript">document.write(String.fromCharCode(';
+for($i=0;$i<strlen($out);$i++)
+{
+	if($i > 0) $javascript .= ',';
+	$javascript .= ord(substr($out,$i,1));
+}
+$javascript .= '));</script>
+<!-- SPAM protection end -->';
+echo $javascript;
 
-<?php 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 if(isset($_FILES['userfile']) && isset($_FILES['userfile']['name']) && trim($_FILES['userfile']['name']) != '')
 {
-	echo '<h2>Result</h2>Dimensions: '.$info[0].' &times; '.$info[1].' pixel<br />Uncompressed Byte-Size (8-Bit Color): '.round(($info[0]*$info[1])/1024).' KB<br /><br />'.$html.'<br /><h2>Size Comparison</h2>'; 
-	echo 'Before: '.round(filesize($uploadfile) / 1024).' KB (source-image file-size)<br />';
-	echo 'After: '.round(strlen($html) / 1024).' KB (source-code byte-size)<br />';
+	echo '<h2>Result</h2>Dimensions: '.$info[0].' &times; '.$info[1].' pixel<br>Uncompressed Byte-Size (8-Bit Color): '.round(($info[0]*$info[1])/1024).' KB<br><br>'.$html.'<br><h2>Size Comparison</h2>'; 
+	echo 'Before: '.round(filesize($uploadfile) / 1024).' KB (source-image file-size)<br>';
+	echo 'After: '.round(strlen($html) / 1024).' KB (source-code byte-size)<br>';
 echo '<h2>Code</h2>
 <textarea cols="200" rows="100" style="width: 95%">'.$html.'</textarea>';
+
+
+	// Email to User
+	if(trim($_POST['email']) != '' && preg_match('/[a-z0-9\.\-_]+@[a-z0-9\.\-_]+/i',$_POST['email']) == 1)
+	{
+$mailhtml = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+'.$html.'
+</body>
+</html>';
+
+		$mail = new PHPMailer();
+		$mail->SetLanguage("en", "language/phpmailer.lang-en.php"); 
+		$mail->IsMail();
+		$mail->IsHTML(true);
+		$mail->From = 'webmaster@pimpmylaptop.de';	
+		$mail->FromName = 'IMG-2-TABLE Script';
+		$mail->AddAddress($_POST['email'],$_POST['email']);
+		$mail->Subject = utf8_decode("IMG-2-TABLE Test");
+		$mail->Body = utf8_decode($mailhtml);
+		$mail->AltBody 	= 'IMG-2-TABLE Test';
+		if (!$mail->Send()) echo '<p>'.$mail->ErrorInfo.'</p>';
+	}
 }
 ?>
-
 </body>
 </html>
